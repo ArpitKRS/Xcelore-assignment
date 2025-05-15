d4f90b072709bc99f255b577cb6159019b3d1b6e
Arpit Kumar Singh <singharpit302@gmail.com>
2025-05-15 23:04:06 +0530

    Documentation (For future frontend)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <p>The Application is running!! :)</p>
+      <p>(Frontend for future use)</p>
     </div>
   );
 }
